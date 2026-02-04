95252ad47f07a20032d3671a738fee0f13929df7
prattakit <rattakit_p@oga.co.th>
2026-02-04 07:52:41 +0000

    fix: แก้ไขโค้ดเรียบร้อย

--- bookingrooms/script.js
+++ bookingrooms/script.js
@@ -2,18 +2,39 @@ function bookRoom() {
   const name = document.getElementById("name").value;
   const room = document.getElementById("room").value;
   const date = document.getElementById("date").value;
+  const table = document.getElementById("bookingList");
 
   if (!name || !room || !date) {
     alert("กรุณากรอกข้อมูลให้ครบ");
     return;
   }
 
-  const table = document.getElementById("bookingList");
+    // 🔍 เช็คซ้ำ
+  for (let i = 0; i < table.rows.length; i++) {
+    const bookedRoom = table.rows[i].cells[1].innerText;
+    const bookedDate = table.rows[i].cells[2].innerText;
+
+    if (bookedRoom === room && bookedDate === date) {
+      alert("ห้องนี้ถูกจองแล้วในวันที่เลือก");
+      return;
+    }
+  }
+
   const row = table.insertRow();
 
   row.insertCell(0).innerText = name;
   row.insertCell(1).innerText = room;
   row.insertCell(2).innerText = date;
+
+  const deleteCell = row.insertCell(3);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "ลบ";
+  deleteBtn.className = "delete-btn";
+  deleteBtn.onclick = function () {
+    row.remove();
+  };
+
+  deleteCell.appendChild(deleteBtn);
 
   document.getElementById("name").value = "";
   document.getElementById("room").value = "";
