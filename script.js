5246ca7815d2bc12b5868360e98e8efa98b7bfa2
prattakit <rattakit_p@oga.co.th>
2026-02-04 10:01:02 +0000

    update all files

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  if (!username || !password) {
-    alert("กรุณากรอก Username และ Password");
-    return;
-  }
-
   fetch("https://script.google.com/macros/s/AKfycbwQW9Kx3jbFbRl2CaieKMB-9bQev4V1OE4kZAzIru_VqmzONkSO1kwzcsOBagITt4KC/exec", {
-
-    
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ username, password })
+    body: JSON.stringify({
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value
+    })
   })
   .then(res => res.json())
   .then(data => {
-    if (data.success) {
-      document.getElementById("result").innerText =
-        "ยินดีต้อนรับ " + data.fullname;
-
-      localStorage.setItem("user", data.fullname);
-
-      // ตัวอย่างเปลี่ยนหน้า
-      // window.location.href = "booking.html";
-    } else {
-      document.getElementById("result").innerText = data.message;
-    }
-  })
-  .catch(() => {
     document.getElementById("result").innerText =
-      "เกิดข้อผิดพลาดในการเชื่อมต่อ";
+      data.success ? "ยินดีต้อนรับ " + data.fullname : data.message;
+  })
+  .catch(err => {
+    console.error(err);
+    document.getElementById("result").innerText =
+      "เชื่อมต่อ API ไม่ได้ (CORS)";
   });
 }
