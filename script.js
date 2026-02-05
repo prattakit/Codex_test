64f7e8b0d979582e8e6ed268db15bb0bb7622f37
prattakit <rattakit_p@oga.co.th>
2026-02-05 02:53:20 +0000

    update all files

--- script.js
+++ script.js
@@ -2,28 +2,24 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  fetch("https://script.google.com/macros/s/1jjS6SuXKc-F6AtUROX822QjGIBbKrb1oc0mOZzFR_B7kW1nyUD4jflDZ/exec", {
+  const url =
+    "https://script.google.com/macros/s/AKfycbxs2Tq8icOCHJRI8Som_YBR_sLw0c_BAE7Ah0d6I5f2NSpB5Jblg9-mSfwswJtc5ffp/exec"
+    + "&username=" + encodeURIComponent(username)
+    + "&password=" + encodeURIComponent(password);
     
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      username: username,
-      password: password
+    
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (data.success) {
+        alert("ยินดีต้อนรับ " + data.fullname);
+      } else {
+        alert(data.message);
+      }
     })
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);  // แสดงผลลัพธ์ใน Console
-    if (data.success) {
-      alert("ยินดีต้อนรับ " + data.fullname);
-    } else {
-      alert(data.message); // แจ้งข้อผิดพลาด
-    }
-  })
-  .catch(err => {
-    alert("เกิดข้อผิดพลาดในการเชื่อมต่อ");
-  });
+    .catch(err => {
+      console.error(err);
+      alert("เกิดข้อผิดพลาดในการเชื่อมต่อ");
+    });
 }
-
