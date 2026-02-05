313ade5f827e212886eaae2063b4d14ff5300dfc
prattakit <rattakit_p@oga.co.th>
2026-02-05 03:00:23 +0000

    update all files

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function login() {
   const password = document.getElementById("password").value;
 
   const url =
-    "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjdGgNLwu99k0NKUj6CLt9TiLATPgDVEqKvPLvhZ_N2R9X_Np4DE7cTgnfMRDSjL-HzU0rqkrqqx0exnrHW9CK_MApvVOX-iF8bC4aUpq38TpZaALljeQSnNw-gh0AekzcXhGlLjOQuT1N1R-JHLaS6Ue-9uybiOKzrKPO4nvALkjiKkbxpsv4bGna0yLMpnyFvjqFHf6Q_tXFXrjYpZrxptIg7TBHlgD1vW5e6grtbCxzRiupQlTnJdGfWn_p0xPYXDagLmVGW1qeolfWnFCP8v61kaA&lib=MqcGTAQ71HOc4wixN6ifat89cFyMI6jSi"
+    "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjdGgNLwu99k0NKUj6CLt9TiLATPgDVEqKvPLvhZ_N2R9X_Np4DE7cTgnfMRDSjL-HzU0rqkrqqx0exnrHW9CK_MApvVOX-iF8bC4aUpq38TpZaALljeQSnNw-gh0AekzcXhGlLjOQuT1N1R-JHLaS6Ue-9uybiOKzrKPO4nvALkjiKkbxpsv4bGna0yLMpnyFvjqFHf6Q_tXFXrjYpZrxptIg7TBHlgD1vW5e6grtbCxzRiupQlTnJdGfWn_p0xPYXDagLmVGW1qeolfWnFCP8v61kaA"
     + "&username=" + encodeURIComponent(username)
     + "&password=" + encodeURIComponent(password);
     
