970ebc2d792420c0085d0e08e6ffc4449b4a06ca
prattakit <rattakit_p@oga.co.th>
2026-02-05 04:02:58 +0000

    update all files

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const url =
-    "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjdGgNLwu99k0NKUj6CLt9TiLATPgDVEqKvPLvhZ_N2R9X_Np4DE7cTgnfMRDSjL-HzU0rqkrqqx0exnrHW9CK_MApvVOX-iF8bC4aUpq38TpZaALljeQSnNw-gh0AekzcXhGlLjOQuT1N1R-JHLaS6Ue-9uybiOKzrKPO4nvALkjiKkbxpsv4bGna0yLMpnyFvjqFHf6Q_tXFXrjYpZrxptIg7TBHlgD1vW5e6grtbCxzRiupQlTnJdGfWn_p0xPYXDagLmVGW1qeolfWnFCP8v61kaA"
+  // const url =
+  //   "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjdGgNLwu99k0NKUj6CLt9TiLATPgDVEqKvPLvhZ_N2R9X_Np4DE7cTgnfMRDSjL-HzU0rqkrqqx0exnrHW9CK_MApvVOX-iF8bC4aUpq38TpZaALljeQSnNw-gh0AekzcXhGlLjOQuT1N1R-JHLaS6Ue-9uybiOKzrKPO4nvALkjiKkbxpsv4bGna0yLMpnyFvjqFHf6Q_tXFXrjYpZrxptIg7TBHlgD1vW5e6grtbCxzRiupQlTnJdGfWn_p0xPYXDagLmVGW1qeolfWnFCP8v61kaA"
+  //   + "&username=" + encodeURIComponent(username)
+  //   + "&password=" + encodeURIComponent(password);
+    
+      const url = "https://script.google.com/macros/s/AKfycbxs2Tq8icOCHJRI8Som_YBR_sLw0c_BAE7Ah0d6I5f2NSpB5Jblg9-mSfwswJtc5ffp/exec?"
     + "&username=" + encodeURIComponent(username)
     + "&password=" + encodeURIComponent(password);
-    
-    
+
   fetch(url)
     .then(res => res.json())
     .then(data => {
