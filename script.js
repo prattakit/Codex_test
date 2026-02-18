c5beb266f9349c3ca4306284eee42ce8ef827cd1
prattakit <rattakit_p@oga.co.th>
2026-02-18 08:00:19 +0000

    update all files

--- script.js
+++ script.js
@@ -2,32 +2,28 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  // const url =
-  // "https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbxs2Tq8icOCHJRI8Som_YBR_sLw0c_BAE7Ah0d6I5f2NSpB5Jblg9-mSfwswJtc5ffp"
-  // + "&username=" + encodeURIComponent(username)
-  // + "&password=" + encodeURIComponent(password);
-    
+  const url =
+    "https://script.google.com/macros/s/AKfycbyItoPAZxwMnqasn0o7Sh3oim1ZUlDpyVTOfQGIGtnP4m_UyUwA0fXrtsUXbs4A0C9x/exec"
+    + "?username=" + encodeURIComponent(username)
+    + "&password=" + encodeURIComponent(password);
 
-    const url =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbyItoPAZxwMnqasn0o7Sh3oim1ZUlDpyVTOfQGIGtnP4m_UyUwA0fXrtsUXbs4A0C9x"
-  + "&username=" + encodeURIComponent(username)
-  + "&password=" + encodeURIComponent(password);
-
-
-
-  
   fetch(url)
-    .then(res => res.json())
+    .then(res => {
+      // กันกรณี server ส่ง HTML กลับมา
+      if (!res.ok) {
+        throw new Error("HTTP error " + res.status);
+      }
+      return res.json();
+    })
     .then(data => {
       console.log(data);
+
       if (data.success) {
-        alert("ยินดีต้อนรับ บบบบ " + data.fullname);
-         if (data.success) {
-            // localStorage.setItem("fullname", data.fullname);
-            navigate("/home");
-          }
+        alert("ยินดีต้อนรับ " + data.fullname);
+        // localStorage.setItem("fullname", data.fullname);
+        navigate("/home");
       } else {
-        alert(data.message);
+        alert(data.message || "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง");
       }
     })
     .catch(err => {
@@ -35,10 +31,3 @@ function login() {
       alert("เกิดข้อผิดพลาดในการเชื่อมต่อ");
     });
 }
-
-
-function logout() {
-  localStorage.clear();
-  navigate("/login");
-}
-
