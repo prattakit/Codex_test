fed99980ccc7fc192a881c333a86cbd588b6787c
prattakit <rattakit_p@oga.co.th>
2026-02-18 07:50:49 +0000

    update all files

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const url =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbxs2Tq8icOCHJRI8Som_YBR_sLw0c_BAE7Ah0d6I5f2NSpB5Jblg9-mSfwswJtc5ffp"
-  + "&username=" + encodeURIComponent(username)
-  + "&password=" + encodeURIComponent(password);
+  // const url =
+  // "https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbxs2Tq8icOCHJRI8Som_YBR_sLw0c_BAE7Ah0d6I5f2NSpB5Jblg9-mSfwswJtc5ffp"
+  // + "&username=" + encodeURIComponent(username)
+  // + "&password=" + encodeURIComponent(password);
     
 
+    const url =
+  "https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbyItoPAZxwMnqasn0o7Sh3oim1ZUlDpyVTOfQGIGtnP4m_UyUwA0fXrtsUXbs4A0C9x"
+  + "&username=" + encodeURIComponent(username)
+  + "&password=" + encodeURIComponent(password);
+
+
+
+  
   fetch(url)
     .then(res => res.json())
     .then(data => {
